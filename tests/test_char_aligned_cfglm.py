4b0c5f728625a648f341449570a0535db8b610c2
Tim Vieira <tim.f.vieira@gmail.com>
2024-04-29 13:02:23 -0400

    added next benchmark for `CharAlignedCFGLM`.

--- tests/test_char_aligned_cfglm.py
+++ tests/test_char_aligned_cfglm.py
@@ -1,10 +1,74 @@
 from genparse.util import LarkStuff, hf_tokenizer
 from genparse import CFGLM, add_EOS, locally_normalize
 from genparse.cfglm import CharAlignedCFGLM
-from arsenal import timeit
+from arsenal import timeit, colors
 
 
-def test_basic_aligned_model():
+def test_basic_aligned_model_arithmetic():
+
+    lark_stuff = LarkStuff(r"""
+    start: WS sum "</s>" | NAME "=" sum "</s>"
+
+    sum: product | sum "+" product | sum MINUS product
+
+    product: atom
+        | product "*" atom
+        | product "/" atom
+
+    atom: NUMBER
+            | MINUS atom
+            | NAME
+            | "(" sum ")"
+
+    MINUS: /[\-]/
+    NUMBER: /[\-+]?\d{1,3}(\.\d{1,3})?/
+    WS: /[ \t\f\r\n]/
+    NAME: /[a-zA-Z_]{1,5}/
+    """)
+
+    with timeit('grammar setup'):
+        foo = lark_stuff.char_cfg(.9)
+        foo = locally_normalize(foo, tol=1e-100).trim()
+        assert len(foo) > 0
+    with timeit('cfglm setup'):
+        # the base character-level CFG language model
+        lm = CFGLM(foo)
+
+    with timeit('tokenizer setup'):
+        H = hf_tokenizer()
+
+    with timeit('CharAlignedCFGLM setup'):
+        bpe_lm = CharAlignedCFGLM(lm=lm, words={x for _, x in H.pairs}, eos=H.tokenizer.eos_token)
+
+
+    #===========================================================================
+    # [2024-04-29 Mon] OPTIMIZATIONS: Tianyu provided the follow contexts, which
+    # include a mix of slow and not so slow strings
+    #
+    #
+    #
+    #===========================================================================
+    contexts = [
+        #'Bey',
+        #'BeyAk',
+        'BeyAk=-',
+        #'BeyAk=-amide',
+        #'BeyAk=-amide*',
+        #'BeyAk=-amide*pread',
+        #'BeyAk=-amide*pread+(',
+        #'BeyAk=-amide*pread+(Dust',
+        #'BeyAk=-amide*pread+(Dusts',
+        #'BeyAk=-amide*pread+(Dusts)--',
+    ]
+
+    for context in contexts:
+        print(colors.yellow % repr(context))
+        with timeit('parsing'):
+            p = bpe_lm.p_next(context)
+            print('output size:', len(p))
+
+
+def test_basic_aligned_model_iql_small():
 
     lark_stuff = LarkStuff(r"""
     start: "SELECT" WS select_expr WS "FROM" WS from_expr [WS "WHERE" WS bool_condition] [WS "GROUP BY" WS var_list] [WS "ORDER BY" WS orderby_expr] WS EOS
