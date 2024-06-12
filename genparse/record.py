3feef793227aa0e9127f30a6ef41820896fbd5ca
Jacob Louis Hoover <postylem@gmail.com>
2024-06-12 17:22:13 -0400

    jac checking in

--- genparse/record.py
+++ genparse/record.py
@@ -22,10 +22,10 @@ class SMCRecord(dict):
     def _repr_html_(self):
         # Just copied from chart.Chart  --- Might not look good.
         return ('<div>SMCRecord:<div><div style="font-family: Monospace;">'
-            + format_table(self.as_df().items(), headings=['key', 'value'])
+            + format_table(self.to_df().items(), headings=['key', 'value'])
             + '</div>')
     
-    def as_df(self, process=True):
+    def to_df(self, process=True):
         df = pd.DataFrame(self)
         if not process: return df
         df['token'] = df['context'].apply(lambda r: [ri[-1] if len(ri)>0 else [] for ri in r ])
@@ -34,7 +34,7 @@ class SMCRecord(dict):
     
     def df_for_plotting(self):
         # Process the data for plotting. 
-        recs = self.as_df(process=True).explode(['context','weight','resampled as','token']).reset_index(drop=True)
+        recs = self.to_df(process=True).explode(['context','weight','resampled as','token']).reset_index(drop=True)
         recs['particle'] = recs.groupby(recs.index // self.n_particles).cumcount()
         recs['context_string'] = recs['context'].apply(lambda r: ''.join(r[:-1]))
         recs['exp_average weight'] = recs['average weight'].apply(lambda r: np.exp(r))
@@ -42,38 +42,37 @@ class SMCRecord(dict):
         recs['prop_exp_weight'] = recs['exp_weight'] / recs['exp_average weight']
         return recs
     
-    def plot_history(self, range_x=None, width=1500, height=600):
+    def plot_particles_trajectory(self, opts=dict(), layout_opts=dict()):
         """
-        Plot the history 
+        Plot the particles' trajectory.
         """
 
         import plotly.express as px
         import plotly.graph_objects as go
 
-        recs = self.df_for_plotting()
+        d_ = self.df_for_plotting()
 
-        # Ensure color consistency by creating a color map
-        pallette = px.colors.n_colors('rgb(255, 0, 0)', 'rgb(0, 125, 255)', len(recs['particle'].unique()), colortype='rgb')
+        pallette = px.colors.n_colors('rgb(255, 0, 0)', 'rgb(0, 125, 255)', len(d_['particle'].unique()), colortype='rgb')
         # pallette = px.colors.cyclical.IceFire
-        color_map = {p: pallette[i % len(pallette)] for i, p in enumerate(recs['particle'].unique())}
+        color_map = {p: pallette[i % len(pallette)] for i, p in enumerate(d_['particle'].unique())}
 
         fig = px.scatter(
-            recs, x="step", y="particle", size='prop_exp_weight', hover_name="token", color='resampled as', text='token',
+            d_, x="step", y="particle", size='prop_exp_weight', hover_name="token", color='resampled as', text='token',
             hover_data=["context_string","weight", "resample?"], opacity=0.2,
             # animation_frame="step",# animation_group='resampled as', 
             color_discrete_map=color_map,
-            range_x=range_x,
-            width=width, height=height,
+            # range_x=range_x, width=width, height=height,
+            **opts
         )
 
-        resample_steps = recs[recs['resample?'] == True]['step'].unique()
-        no_resample_steps = recs[recs['resample?'] == False]['step'].unique()
+        resample_steps = d_[d_['resample?'] == True]['step'].unique()
+        no_resample_steps = d_[d_['resample?'] == False]['step'].unique()
         for step in resample_steps:
             fig.add_vline(x=step, line_width=4, opacity=0.15, line_color="gray")
 
         # Add lines to represent resampling
-        for resampled_as in recs['resampled as'].unique():
-            resampled_as_data = recs[recs['resampled as'] == resampled_as]
+        for resampled_as in d_['resampled as'].unique():
+            resampled_as_data = d_[d_['resampled as'] == resampled_as]
             
             for step in resample_steps:
                 for _, row in resampled_as_data[resampled_as_data['step'] == step].iterrows():
@@ -93,6 +92,7 @@ class SMCRecord(dict):
             #             x=[row['step']+1/2, row['step']+1], y=[row['particle'], row['particle']],
             #             mode='lines', line=dict(color='gray'), opacity=0.2, name=resampled_as, showlegend=False))
         fig.update_traces(textposition='middle right')
-        fig.update_layout(plot_bgcolor="#fff", showlegend=False, )
+        fig.update_layout(plot_bgcolor="#fff", showlegend=False, margin=dict(l=0, r=0, t=15, b=0), **layout_opts)
         fig.update_yaxes(showticklabels=False, visible=False)
-        fig.show()
+
+        return fig
