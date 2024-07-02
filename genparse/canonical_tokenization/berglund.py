269d4cc19fd8a3c9e873b64f13652d3d7d34995f
Brian DuSell <brian.dusell@inf.ethz.ch>
2024-07-02 12:53:06 +0000

    Remove all transitions for states with in-degree 0.

--- genparse/canonical_tokenization/berglund.py
+++ genparse/canonical_tokenization/berglund.py
@@ -3,11 +3,14 @@ import itertools
 import sys
 from collections.abc import Iterable, Sequence
 
+
 State = int
 Symbol = int
 # A merge rule consists of (u, v, uv), where uv is the index of the symbol for
 # the concatenation of u and v.
 MergeRule = tuple[Symbol, Symbol, Symbol]
+
+_NO_TRANSITIONS = {}
 
 
 @dataclasses.dataclass
@@ -16,6 +19,7 @@ class TokenDFA:
     # Note that in this construction, all states are accept states, so there
     # is no explicit set of accept states.
     transitions: list[dict[Symbol, State]]
+    in_degree: list[int]
 
     @staticmethod
     def from_dictionary(
@@ -31,7 +35,10 @@ class TokenDFA:
 
     @staticmethod
     def from_base_alphabet(base_alphabet: Iterable[Symbol]) -> 'TokenDFA':
-        return TokenDFA(transitions=[{a: 0 for a in base_alphabet}])
+        transitions_from_init = {a: 0 for a in base_alphabet}
+        return TokenDFA(
+            transitions=[transitions_from_init], in_degree=[len(transitions_from_init)]
+        )
 
     def states(self) -> range:
         return range(len(self.transitions))
@@ -40,6 +47,7 @@ class TokenDFA:
         prev_len = len(self.transitions)
         for _ in range(n):
             self.transitions.append({})
+            self.in_degree.append(0)
         return range(prev_len, len(self.transitions))
 
     def get_state_to(self, state_from: State, symbol: Symbol) -> State | None:
@@ -55,8 +63,32 @@ class TokenDFA:
             for symbol, state_to in transitions_from_state.items():
                 yield state_from, symbol, state_to
 
-    def set_transition(self, state_from: State, symbol: Symbol, state_to: State) -> None:
+    def add_transition(self, state_from: State, symbol: Symbol, state_to: State) -> None:
+        """Precondition: state_from does not already have a transition on symbol."""
         self.transitions[state_from][symbol] = state_to
+        self.in_degree[state_to] += 1
+
+    def reset_transition(
+        self, state_from: State, symbol: Symbol, state_to: State
+    ) -> None:
+        """Precondition: state_from already has a transition on symbol."""
+        self.in_degree[self.transitions[state_from][symbol]] -= 1
+        self.add_transition(state_from, symbol, state_to)
+
+    def check_state_for_removal(self, state: State) -> None:
+        if self.in_degree[state] == 0:
+            agenda = [state]
+            discovered = set(agenda)
+            # NOTE: No states are ever removed recursively in practice. Is it
+            # even possible for this to be necessary?
+            while agenda:
+                state = agenda.pop()
+                for state_to in self.transitions[state].values():
+                    self.in_degree[state_to] -= 1
+                    if self.in_degree[state_to] == 0 and state_to not in discovered:
+                        discovered.add(state_to)
+                        agenda.append(state_to)
+                self.transitions[state] = _NO_TRANSITIONS
 
     def merge_rule(self, rule: MergeRule) -> None:
         # This implements Algorithm 2 of https://arxiv.org/pdf/2405.07671
@@ -68,7 +100,7 @@ class TokenDFA:
             if s2 is not None:
                 s3 = self.get_state_to(s2, v)
                 if s3 is not None:
-                    self.set_transition(s1, uv, s3)
+                    self.add_transition(s1, uv, s3)
                     S2[s2] = True
         # If S2 is empty, the rest of this algorithm is a no-op. Stop early to
         # save time.
@@ -81,11 +113,12 @@ class TokenDFA:
         for s2, fresh_s2 in zip(S2, fresh):
             for alpha, state_to in self.get_transitions_from_state(s2):
                 if alpha not in excluded:
-                    self.set_transition(fresh_s2, alpha, state_to)
+                    self.add_transition(fresh_s2, alpha, state_to)
         state_to_fresh = dict(zip(S2, fresh))
         for q in self.states():
             state_to = self.get_state_to(q, u)
             if state_to is not None:
                 fresh_state_to = state_to_fresh.get(state_to)
                 if fresh_state_to is not None:
-                    self.set_transition(q, u, fresh_state_to)
+                    self.reset_transition(q, u, fresh_state_to)
+                    self.check_state_for_removal(state_to)
