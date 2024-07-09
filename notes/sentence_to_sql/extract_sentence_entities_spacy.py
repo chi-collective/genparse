cff16b0b0a57aeb2a353a071b57b65b906b28ec6
Joe Cecil <jcecil@isi.edu>
2024-07-09 15:14:24 -0400

    Fix uniquification

--- notes/sentence_to_sql/extract_sentence_entities_spacy.py
+++ notes/sentence_to_sql/extract_sentence_entities_spacy.py
@@ -36,7 +36,7 @@ def _uniquify(items):
     """
     result = []
     for item in items:
-        if item not in items:
+        if item not in result:
             result.append(item)
     return result
 
