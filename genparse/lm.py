Correct LLM probabilities calculation

genparse/lm.py
@@ -114,12 +114,16 @@ class LLM(LM):
     # TODO: cover me!
     def __call__(self, input_ids):
         if isinstance(input_ids, list):
-            input_ids = torch.LongTensor([input_ids])
+            input_ids = torch.LongTensor([input_ids]).squeeze()
+        if input_ids[0] != self.model.config.bos_token_id:
+            input_ids = torch.cat(
+                [torch.LongTensor([self.model.config.bos_token_id]), input_ids]
+            )
         with torch.no_grad():
             input_ids = input_ids.to(self.device)
             outputs = self.model(input_ids=input_ids, labels=input_ids)
             lprobs = torch.nn.functional.log_softmax(outputs.logits, dim=-1)
-        token_lprobs = torch.gather(lprobs, 2, input_ids.unsqueeze(-1)).squeeze(-1)
+        token_lprobs = torch.gather(lprobs, 1, input_ids[1:].unsqueeze(-1)).squeeze(-1)
         return np.exp(torch.sum(token_lprobs, dim=-1).item())
 
     def clear_cache(self):