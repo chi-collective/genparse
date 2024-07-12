ef5c7f755b9217e2e50c148d95f1fa988de4f06f
Joe Cecil <jcecil@isi.edu>
2024-07-12 13:28:20 -0400

    Preprocess Wikidata before importing it

--- notes/sentence_to_sql/import_wikidata_db.sh
+++ notes/sentence_to_sql/import_wikidata_db.sh
@@ -6,8 +6,11 @@ if [[ "$#" -ne 2 ]]; then
   printf '%s\n' "usage: $0 csv_path db_path"
 fi
 
-csv_path=$1
+raw_csv_path=$1
 db_path=$2
+
+csv_path="${raw_csv_path%.csv}_preprocessed.csv"
+python preprocess_wikidata.py "$raw_csv_path" "$csv_path"
 
 export csv_path
 cat "$script_root"/import_wikidata_db.sql |
---
+++ notes/sentence_to_sql/preprocess_wikidata.py
@@ -0,0 +1,98 @@
+"""
+Preprocess Wikidata dumps into a nicer form for Code Llama's use.
+"""
+
+from argparse import ArgumentParser
+import csv
+import logging
+from pathlib import Path
+from typing import Any, Iterable
+
+logger = logging.getLogger(__name__)
+
+
+DROP_COLUMNS = (
+    '',
+    'Unnamed: 0',
+    'imageLabel.value',
+    'instance_ofLabel.value',
+    'Commons_categoryLabel.value',
+    'ISNILabel.value',
+    'signatureLabel.value',
+    'described_by_sourceLabel.value',
+    'topic_s_main_categoryLabel.value',
+    'Commons_galleryLabel.value',
+)
+# These suffixes on a column name indicate it's a "data type" or "language" attribute.
+# We don't care about those.
+TYPE_TAG_SUFFIX = '.type'
+XML_LANG_SUFFIX = '.xml:lang'
+
+
+def choose_columns_to_drop(fieldnames: Iterable[str]) -> set[str]:
+    result = set(DROP_COLUMNS)
+    result.update(
+        column
+        for column in fieldnames
+        if column.endswith(TYPE_TAG_SUFFIX) or column.endswith(XML_LANG_SUFFIX)
+    )
+    return result
+
+
+def clean_column(column: str) -> str:
+    """Clean the given column name."""
+    return column.removesuffix('Label.value')
+
+
+def main():
+    parser = ArgumentParser(description=__doc__)
+    parser.add_argument(
+        'csv_in',
+        type=Path,
+        help='Path to the CSV Wikidata dump to preprocess.',
+    )
+    parser.add_argument(
+        'csv_out',
+        type=Path,
+        help='Path to the CSV Wikidata dump to preprocess.',
+    )
+    parser.add_argument(
+        '--logging-level',
+        type=str,
+        default='INFO',
+        help='Logging level to use.',
+    )
+    args = parser.parse_args()
+
+    logging.basicConfig(
+        level=getattr(logging, args.logging_level),
+        format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S',
+    )
+
+    csv_in_path: Path = args.csv_in
+    csv_out_path: Path = args.csv_out
+
+    with (
+        csv_in_path.open(mode='r', encoding='utf-8', newline='') as csv_in,
+        csv_out_path.open(mode='w', encoding='utf-8', newline='') as csv_out,
+    ):
+        reader = csv.DictReader(csv_in)
+        writer = csv.DictWriter(csv_out, fieldnames=reader.fieldnames)
+        columns_to_drop = choose_columns_to_drop(reader.fieldnames)
+        # PyCharm gets confused and thinks the rows are strings
+        row: dict[str, Any]
+        for row in reader:
+            kept_columns_only = {
+                column: value
+                for column, value in row.items()
+                if column not in columns_to_drop
+            }
+            cleaned = {
+                clean_column(column): value for column, value in kept_columns_only.items()
+            }
+            writer.writerow(cleaned)
+
+
+if __name__ == '__main__':
+    main()
