c4279a79fbb0a3fe2bee0eefa33ba2047977c8bb
benlebrun <benlebrun1@gmail.com>
2024-07-16 16:37:33 +0000

    local poe benchmarking

--- benchmark/run_spider_genparse.py
+++ benchmark/run_spider_genparse.py
@@ -17,6 +17,8 @@ from tqdm import tqdm
 
 from genparse.cfglm import BoolCFGLM
 from genparse.lm import TokenizedLLM
+from genparse.util import lark_guide
+from genparse.experimental.steer_local import LocalPOESampler
 from genparse.backends.vllm import vllmpplLLM, VLLMSampler
 from genparse.proposal import CharacterProposal
 from genparse.util import LarkStuff, set_seed
@@ -41,7 +43,7 @@ def get_parser() -> argparse.ArgumentParser:
     parser.add_argument('--exp-name', type=str, default='llama3-8b-100')
     parser.add_argument(
         '--inference',
-        choices=['smc-standard', 'smc-steer', 'importance-sampling'],
+        choices=['smc-standard', 'smc-steer', 'importance-sampling', 'local-poe'],
         default='smc-standard',
     )
     parser.add_argument('--particles', type=int, default=1)
@@ -49,8 +51,24 @@ def get_parser() -> argparse.ArgumentParser:
     parser.add_argument('--max-tokens', type=int, default=100)
     parser.add_argument('--verbosity', type=int, default=0)
     parser.add_argument('--seed', type=int, default=0)
+    parser.add_argument('--K', type=int, default=20)
 
     return parser
+
+
+def reformat_grammar(grammar):
+    """move start rule and remove zero-width rules"""
+    lines = grammar.split('\n')
+    new_grammar = ''
+    for line in lines:
+        if line == '|""i':
+            continue
+        if line.startswith('start'):
+            new_grammar = line + '\n' + new_grammar
+        else:
+            new_grammar += line + '\n'
+
+    return new_grammar
 
 
 def main():
@@ -83,21 +101,30 @@ def main():
     evaluator = Evaluator(raw_spider_dir)
 
     # Initialize model.
-    logger.info(f'Initializing model: {args.model_name} ...')
-    hfppl_llm = vllmpplLLM(args.model_name, max_model_len=4096, seed=args.seed)
-    tokenizer = transformers.AutoTokenizer.from_pretrained(args.model_name)
-    with open('benchmark/grammars/sql_case_insensitive.lark', 'r') as f:
-        grammar = f.read()
 
     BATCH_SIZE = 80
 
-    hfppl_llm.batch_size = BATCH_SIZE
-    genparse_llm = TokenizedLLM(
-        model=hfppl_llm, tokenizer=tokenizer, batch_size=BATCH_SIZE
-    )
-    guide = BoolCFGLM(LarkStuff(grammar).char_cfg(0.99))
-    sampler = VLLMSampler(llm=genparse_llm, guide=guide)
-    proposal = CharacterProposal(llm=genparse_llm, guide=guide)
+    logger.info(f'Initializing model: {args.model_name} ...')
+
+    if args.inference == 'local-poe':
+        logger.info('Using local product of experts')
+        from vllm import LLM
+
+        vllm_llm = LLM(model=args.model_name)
+        tokenizer = vllm_llm.get_tokenizer()
+    else:
+        hfppl_llm = vllmpplLLM(args.model_name, max_model_len=4096, seed=args.seed)
+        hfppl_llm.batch_size = BATCH_SIZE
+        tokenizer = transformers.AutoTokenizer.from_pretrained(args.model_name)
+        with open('benchmark/grammars/sql_case_insensitive.lark', 'r') as f:
+            grammar = f.read()
+        genparse_llm = TokenizedLLM(
+            model=hfppl_llm, tokenizer=tokenizer, batch_size=BATCH_SIZE
+        )
+        guide = BoolCFGLM(LarkStuff(grammar).char_cfg(0.99))
+        sampler = VLLMSampler(llm=genparse_llm, guide=guide)
+        proposal = CharacterProposal(llm=genparse_llm, guide=guide)
+
     logger.info('Model initialized.')
 
     # Setup saving.
@@ -108,8 +135,21 @@ def main():
 
     n_correct, n_invalid, n_mismatch = 0, 0, 0
 
+    guides = {}
+
+    grammars = json.load(open('benchmark/grammars/spider_schema_grammar.json', 'r'))
+
     for i, dev_datum in tqdm(enumerate(spider_dev_data[:n_query]), total=n_query):
         messages = prompt_formatter.format_openai(dev_datum)
+
+        if dev_datum.schema_name in guides:
+            guide = guides[dev_datum.schema_name]
+        else:
+            if dev_datum.schema_name in grammars:
+                guide = lark_guide(reformat_grammar(grammars[dev_datum.schema_name]))
+                guides[dev_datum.schema_name] = guide
+            else:
+                print(f'skipping {dev_datum.schema_name}')
 
         if i == 0:  # print an example for demonstration
             print('=' * 30 + ' Example prompt ' + '=' * 30)
@@ -123,15 +163,25 @@ def main():
         prompt = tokenizer.decode(
             tokenizer.apply_chat_template(messages, add_generation_prompt=True)
         )
-        particles = sampler.run_inference(
-            prompt=prompt,
-            proposal=proposal,
-            method=args.inference,
-            n_particles=args.particles,
-            max_tokens=args.max_tokens,
-            n_beam=args.n_beam,
-            verbosity=args.verbosity,
-        )
+
+        if args.inference == 'local-poe':
+            sampler = LocalPOESampler(vllm_llm, guide, K=args.K)
+            particles = sampler.run_inference(
+                prompt=prompt,
+                n_particles=args.particles,
+                max_tokens=args.max_tokens,
+                seed=args.seed,
+            )
+        else:
+            particles = sampler.run_inference(
+                prompt=prompt,
+                proposal=proposal,
+                method=args.inference,
+                n_particles=args.particles,
+                max_tokens=args.max_tokens,
+                n_beam=args.n_beam,
+                verbosity=args.verbosity,
+            )
 
         pmax = particles.particles[0]
         for p in particles.particles[1:]:
