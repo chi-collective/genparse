87edec4785c1ac164b50d94c5a505ee2f87b2cb3
Tim Vieira <tim.f.vieira@gmail.com>
2024-07-18 17:52:59 -0400

    ()

--- benchmark/run.py
+++ benchmark/run.py
@@ -3,17 +3,19 @@ from example_grammars import arith, iql_small
 from genparse import locally_normalize
 from genparse.parse.earley import EarleyLM
 from genparse.proposal import CharacterProposal, TokenProposal
-from genparse.util import LarkStuff, set_seed, load_model_by_name
+from genparse.util import set_seed, load_model_by_name
+from genparse.lark_interface import LarkStuff
 
 
 def test_token_arith():
     set_seed(0)
 
+    llm = load_model_by_name('mock-gpt2')
+
     cfg = locally_normalize(LarkStuff(arith, cnf=False).char_cfg(), tol=1e-100).trim()
 
     guide = EarleyLM(cfg)
-
-    llm = load_model_by_name('mock-gpt2')
+    guide.V |= {w for word in llm.V for w in word}
 
     proposal = TokenProposal(guide=guide, llm=llm, K=5)
 
@@ -26,11 +28,12 @@ def test_token_arith():
 def test_character_arith():
     set_seed(0)
 
+    llm = load_model_by_name('mock-gpt2')
+
     cfg = locally_normalize(LarkStuff(arith, cnf=False).char_cfg(), tol=1e-100).trim()
 
     guide = EarleyLM(cfg)
-
-    llm = load_model_by_name('mock-gpt2')
+    guide.V |= {w for word in llm.V for w in word}
 
     proposal = CharacterProposal(llm=llm, guide=guide)
 
@@ -43,17 +46,18 @@ def test_character_arith():
 def test_token_iql_small():
     set_seed(0)
 
-    cfg = locally_normalize(LarkStuff(iql_small, cnf=False).char_cfg(), tol=1e-100).trim()
-
     llm = load_model_by_name('mock-gpt2')
 
+    cfg = locally_normalize(LarkStuff(iql_small, cnf=False).char_cfg(), tol=1e-100).trim()
+
     guide = EarleyLM(cfg)
+    guide.V |= {w for word in llm.V for w in word}
 
     proposal = TokenProposal(llm=llm, guide=guide, K=5)
 
     samples = []
     for _ in range(10):
-        samples.append(proposal.sample(chunked=True, max_tokens=100))
+        samples.append(proposal.sample(max_tokens=100))
         print(samples[-1])
 
 
@@ -65,6 +69,7 @@ def test_character_iql_small():
     cfg = locally_normalize(LarkStuff(iql_small, cnf=False).char_cfg(), tol=1e-100).trim()
 
     guide = EarleyLM(cfg)
+    guide.V |= {w for word in llm.V for w in word}
 
     proposal = CharacterProposal(llm=llm, guide=guide)
 
