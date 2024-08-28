769e244a3cbb392370f29990bb951530af499627
Leo Du <leodu2006@gmail.com>
2024-08-28 04:59:11 +0000

    benchmark fast earley on spider

--- benchmark/sql_grammar_coverage.py
+++ benchmark/sql_grammar_coverage.py
@@ -1,14 +1,20 @@
 import argparse
 import logging
-from genparse.cfglm import BoolMaskCFGLM
-from genparse.util import LarkStuff
+from genparse.lark_interface import LarkStuff
+
+from tqdm import tqdm
 
 logger = logging.getLogger(__name__)
 
 
-def load_guide(grammar_name):
+def load_guide(grammar_name, use_fast=False):
     cfg = LarkStuff(open(grammar_name).read()).char_cfg()
-    return BoolMaskCFGLM(cfg)
+    if use_fast:
+        print('Using earley_fast')
+        from earley_fast import BoolCFGLM
+    else:
+        from genparse.cfglm import BoolCFGLM
+    return BoolCFGLM(cfg)
 
 
 def load_examples(example_path):
@@ -23,10 +29,10 @@ def main(grammar_path, example_path, out_path):
     logger.info(f'guide : {grammar_path}')
     logger.info(f'examples : {example_path}')
 
-    guide = load_guide(grammar_path)
+    guide = load_guide(grammar_path, args.use_fast)
     exmps = load_examples(example_path)
 
-    for ex_id, entry in enumerate(exmps):
+    for ex_id, entry in enumerate(tqdm(exmps)):
         if not guide.p_next(entry):
             logger.info(f'FAILED {ex_id} : {entry}')
             for i in range(1, len(entry)):
@@ -51,6 +57,7 @@ if __name__ == '__main__':
         default='../benchmark/datasets/spider_dev_set.txt',
     )
     parser.add_argument('--out_path', type=str, help='output file')
+    parser.add_argument('--use-fast', action='store_true')
 
     args = parser.parse_args()
     main(args.grammar_path, args.example_path, args.out_path)
